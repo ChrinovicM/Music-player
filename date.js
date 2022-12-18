c0bca58a959dabb60c414def1a5b11885e13f743
ChrinovicM <chrinovicmukulu93@gmail.com>
2022-12-18 00:40:52 -0500

    first commit

---
+++ date.js
@@ -0,0 +1,20 @@
+const songs = [
+    {
+        name: 'Song 1',
+        artist: 'Artist 1',
+        cover:
+        path: 
+    },
+    {
+        name: 'Song 2',
+        artist: 'Artist 2',
+        cover: 
+        path: 
+    },
+    {
+        name: 'Song 3',
+        artist: 'Artist 3',
+        cover: 
+        path: 
+    },
+];
